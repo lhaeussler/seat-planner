c7212d7786c28dcf18916b20a53c40b4525481bf
Leonardo <leonardo.haeussler@addmore.de>
2021-08-18 11:10:18 +0200

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <headmenu></headmenu>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Headmenu from "./components/headmenu";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Headmenu
   }
 }
 </script>
---
+++ src/components/headmenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="headmenu">
+    <b-row align-v="end">
+      <b-col>
+        <b-button size="md" variant="success">Login</b-button>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "headmenu"
+}
+</script>
+
+<style scoped>
+
+</style>
